13e49c5104c613f3820953cb6b98290613bb0981
Vyacheslav Popov <vyacheslav.popov@devexperts.com>
2020-05-05 14:53:35 +0400

    add simple realisation

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <Table />
+</template>
+
+<script>
+import Table from './components/Table.vue';
+
+export default {
+  name: 'App',
+  components: { Table },
+};
+</script>
---
+++ src/components/Table.vue
@@ -0,0 +1,49 @@
+<template>
+  <b-container>
+    <b-table striped bordered hover :items="items" :fields="fields">
+      <template v-slot:cell(flag)="data">
+        <img :src="data.value" alt="Flag" width="32px" />
+      </template>
+    </b-table>
+  </b-container>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      endpoint: 'https://corona.lmao.ninja/v2/countries',
+      items: [],
+      fields: [
+        {
+          key: 'country',
+          label: 'Country',
+        },
+        {
+          key: 'cases',
+          label: 'Cases',
+        },
+        {
+          key: 'flag',
+          label: 'Flag',
+          formatter: 'flagFormatter',
+        },
+      ],
+    };
+  },
+
+  methods: {
+    flagFormatter(value, key, item) {
+      return item.countryInfo.flag;
+    },
+
+    async getData() {
+      this.items = await fetch(this.endpoint).then((res) => res.json());
+    },
+  },
+
+  created() {
+    this.getData();
+  },
+};
+</script>
